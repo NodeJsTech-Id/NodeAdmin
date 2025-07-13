b8e914555b188650f631a064da7f15dbf322bc05
syahrulR11 <muhamad.syahrul.ramadhan@flazhost.com>
2025-07-13 14:08:01 +0700

    Update

--- src/modules/setting/migrations/1752390255273-addNullableMailSettings.ts
+++ src/modules/setting/migrations/1752390255273-addNullableMailSettings.ts
@@ -1,95 +1,81 @@
 import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
-export class AddEmailSettingToSettingsTable1723948121171 implements MigrationInterface {
+export class AddNullableMailSettings1752390255273 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.addColumn(
+        await queryRunner.changeColumn(
             "settings",
+            "mail_from_address",
             new TableColumn({
                 name: "mail_from_address",
                 type: "varchar",
-                length: "100"
+                length: "100",
+                isNullable: true
             }),
         )
-        await queryRunner.addColumn(
+        await queryRunner.changeColumn(
             "settings",
+            "mail_from_name",
             new TableColumn({
                 name: "mail_from_name",
                 type: "varchar",
-                length: "100"
+                length: "100",
+                isNullable: true
             }),
         )
-        await queryRunner.addColumn(
+        await queryRunner.changeColumn(
             "settings",
+            "mail_mailer",
             new TableColumn({
                 name: "mail_mailer",
                 type: "varchar",
-                length: "100"
+                length: "100",
+                isNullable: true
             }),
         )
-        await queryRunner.addColumn(
+        await queryRunner.changeColumn(
             "settings",
+            "mail_host",
             new TableColumn({
                 name: "mail_host",
                 type: "varchar",
-                length: "100"
+                length: "100",
+                isNullable: true
             }),
         )
-        await queryRunner.addColumn(
+        await queryRunner.changeColumn(
             "settings",
+            "mail_port",
             new TableColumn({
                 name: "mail_port",
                 type: "varchar",
-                length: "100"
+                length: "100",
+                isNullable: true
             }),
         )
-        await queryRunner.addColumn(
+        await queryRunner.changeColumn(
             "settings",
+            "mail_username",
             new TableColumn({
                 name: "mail_username",
                 type: "varchar",
-                length: "100"
+                length: "100",
+                isNullable: true
             }),
         )
-        await queryRunner.addColumn(
+        await queryRunner.changeColumn(
             "settings",
+            "mail_password",
             new TableColumn({
                 name: "mail_password",
                 type: "varchar",
-                length: "100"
+                length: "100",
+                isNullable: true
             }),
         )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropColumn(
-            "settings",
-            "mail_from_address"
-        )
-        await queryRunner.dropColumn(
-            "settings",
-            "mail_from_name"
-        )
-        await queryRunner.dropColumn(
-            "settings",
-            "mail_mailer"
-        )
-        await queryRunner.dropColumn(
-            "settings",
-            "mail_host"
-        )
-        await queryRunner.dropColumn(
-            "settings",
-            "mail_port"
-        )
-        await queryRunner.dropColumn(
-            "settings",
-            "mail_username"
-        )
-        await queryRunner.dropColumn(
-            "settings",
-            "mail_password"
-        )
     }
 
 }
